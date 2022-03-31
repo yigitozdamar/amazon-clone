c2b2c9ba3331e0c3472a4235baf9f2d3af88b9ff
yigitozdamar <yigitozdamar@hotmail.com>
2022-03-31 23:00:49 +0300

    home page updated

--- src/Home.js
+++ src/Home.js
@@ -19,7 +19,7 @@ const Home = () => {
             image={
               "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_Electronics_2x._SY608_CB432774322_.jpg"
             }
-            rating={2}
+            rating={3}
           />
           <Product
             id={12346}
@@ -33,40 +33,40 @@ const Home = () => {
         </div>
         <div className="home__row">
           <Product
-            id={12345}
-            title="The Lean Startup"
-            price={29.99}
+            id={12347}
+            title="BENGOO G9000 Stereo Gaming Headset for PS4 PC Xbox One PS5 Controller, Noise Cancelling Over Ear Headphones with Mic, LED Light, Bass Surround"
+            price={49.99}
             image={
-              "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_Electronics_2x._SY608_CB432774322_.jpg"
+              "https://m.media-amazon.com/images/I/61CGHv6kmWL._AC_UY436_FMwebp_QL65_.jpg"
             }
-            rating={2}
+            rating={4}
           />
           <Product
-            id={12346}
-            title="KIZEN Digital Meat Thermometer with Probe - Waterproof, Kitchen Instant Read Food Thermometer for Cooking, Baking, Liquids, Candy, Grilling BBQ "
-            price={13.59}
+            id={12348}
+            title="Seiko 5 Men's Stainless Steel Watch"
+            price={92.58}
             image={
-              "https://m.media-amazon.com/images/I/51+06GSt5xL._AC_SY460_.jpg"
+              "https://m.media-amazon.com/images/I/616g3HenkfL._AC_SY200_.jpg"
             }
             rating={5}
           />
           <Product
-            id={12346}
-            title="KIZEN Digital Meat Thermometer with Probe - Waterproof, Kitchen Instant Read Food Thermometer for Cooking, Baking, Liquids, Candy, Grilling BBQ "
-            price={13.59}
+            id={12349}
+            title="WowWee Pinkfong Baby Shark Official Song Cube - Baby Shark, 3 inches"
+            price={10.01}
             image={
               "https://m.media-amazon.com/images/I/51+06GSt5xL._AC_SY460_.jpg"
             }
-            rating={5}
+            rating={1}
           />
         </div>
         <div className="home__row">
           <Product
             id={12346}
-            title="KIZEN Digital Meat Thermometer with Probe - Waterproof, Kitchen Instant Read Food Thermometer for Cooking, Baking, Liquids, Candy, Grilling BBQ "
-            price={13.59}
+            title="POWERUP 4.0 The Next-Generation Smartphone Controlled Paper Airplane Kit, RC Controlled. Easy to Fly with Autopilot & Gyro Stabilizer."
+            price={79.99}
             image={
-              "https://m.media-amazon.com/images/I/51+06GSt5xL._AC_SY460_.jpg"
+              "https://images-na.ssl-images-amazon.com/images/I/61dOUQ+1xfL._AC_UL320_SR320,320_.jpg"
             }
             rating={5}
           />
